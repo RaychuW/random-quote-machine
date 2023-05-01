6d22be81384e039fb57bb6429f53b8179d14131c
RaychuW <rb.alhamdan@gmail.com>
2023-05-01 09:45:15 +0300

    1.0 done

--- src/features/quote/Quote.js
+++ src/features/quote/Quote.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import {
   callFetchQuote,
@@ -9,14 +9,21 @@ import styles from './Quote.module.css';
 export function Quote() {
   const quote = useSelector(selectQuote);
   const dispatch = useDispatch();
-  // console.log(quote);
+
+  useEffect(() => {
+    dispatch(callFetchQuote());
+  }, [dispatch]);
+
+
   return (
-    <div id="quote-box">
-      <blockquote id="text">
-        {quote.text}<br></br>
-        <cite>- {quote.author}</cite>
-      </blockquote>
-      <button id="new-quote" onClick={() => dispatch(callFetchQuote())}>New Quote</button>
+    <div id='quote-box' className={styles.card}>
+      <div id='text' className={styles.text}>
+        {quote.status === 'loading' && 'Loading...'}
+        {quote.status === 'idle' && quote.text}
+      </div>
+      <cite id='author' className={styles.author}>- {quote.status === 'idle' && quote.author}</cite>
+      <button id='new-quote' className={styles.button} onClick={() => dispatch(callFetchQuote())}>New Quote</button>
+      <a id='tweet-quote' className={styles.tweet} target='_blank' rel='noreferrer' href={'https://twitter.com/intent/tweet?text=' + quote.text + '%0D%0A- ' + quote.author}><i className='fa fa-twitter'></i></a>
     </div>
   );
 }
